79fed31e5b84f1196b070745ee69223998cfc11f
Mananovitz <48227541+Mananovitz@users.noreply.github.com>
2022-02-11 11:20:38 +0000

    Update views.py

--- rango/views.py
+++ rango/views.py
@@ -5,4 +5,4 @@ def index(request):
   return HttpResponse("Rango says hey there partner!") <a href="/rango/about/">about<a>
 
 def about(request):
-  return HttpResponse("Rango says here is the about page.")
+  return HttpResponse("Rango says here is the about page.") <a href="index.htm">Home</a>
