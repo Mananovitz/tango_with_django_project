fa285c3ef6d398bb222a738e46420f615939f340
Mananovitz <48227541+Mananovitz@users.noreply.github.com>
2022-02-11 11:00:00 +0000

    Update urls.py

--- rango/urls.py
+++ rango/urls.py
@@ -4,6 +4,7 @@ from rango import views
 app_name = 'rango'
 
 urlpatterns = [
-path('', views.index, name='index'),
+  path('', views.index, name='index'),
+  path('/rango/about/, views.index, name='about'),
 ]
 
